63f76731e2dc1fe3a39e0e34728d030b2ccb7ec4
sconnock <sconnock@salesforce.com>
2022-11-28 11:34:49 +0000

    Initial commit

---
+++ lwc-helloworld-main/src/app.js
@@ -0,0 +1,13 @@
+import { LightningElement, track } from "lwc";
+
+export default class HelloLwc extends LightningElement {
+  @track greeting = "World";
+
+  handleChange(event) {
+    this.greeting = event.target.value;
+  }
+
+  count() {
+    alert(this.greeting.length + " letters");
+  }
+}
