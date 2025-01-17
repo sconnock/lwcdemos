a22dd98692ea5b7a2f737b16471000a6d6cd725f
sconnock <sconnock@salesforce.com>
2025-01-17 15:06:41 +0000

    Update demo11Bubbling.js

--- demo11Bubbling/demo11Bubbling/demo11Bubbling.js
+++ demo11Bubbling/demo11Bubbling/demo11Bubbling.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class Demo11Bubbling extends LightningElement {}
+export default class Demo11Bubbling extends LightningElement {
+
+    handleNotify(event) {
+        console.log("Parent: Event sent by " + event.target.localName);
+    }
+    
+
+    parentDivNotify(event) {
+        console.log("parentDiv: Event sent by " + event.target.localName);
+    }
+}
