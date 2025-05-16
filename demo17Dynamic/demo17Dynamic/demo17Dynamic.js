1f5b3451515000e1b5ece61c25e6bbdefd536229
sconnock <sconnock@salesforce.com>
2025-05-16 07:52:39 +0100

    Update demo17Dynamic.js

--- demo17Dynamic/demo17Dynamic/demo17Dynamic.js
+++ demo17Dynamic/demo17Dynamic/demo17Dynamic.js
@@ -1,21 +1,22 @@
 import { LightningElement } from 'lwc';
 
 export default class Demo17Dynamic extends LightningElement {
-    componentConstructor;
+    componentConstructor1;
+    componentConstructor2;
 
     // handleClick() {
     //     import("c/demo17Child1")
-    //     .then(({default: ctor}) => (this.componentConstructor = ctor))
+    //     .then(({default: ctor}) => (this.componentConstructor1 = ctor))
     //     .catch((error) => console.log("Error importing component"));
     // }
 
     async handleClick() {
         if (this.refs.toggle.checked) {
             const { default: ctor } = await import("c/demo17Child1");
-            this.componentConstructor = ctor;
+            this.componentConstructor1 = ctor;
         } else {
             const { default: ctor } = await import("c/demo17Child2");
-            this.componentConstructor = ctor;
+            this.componentConstructor2 = ctor;
         }
     }
 }
